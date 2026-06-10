cb43924c371d051f9acc84a88171e4f26e0a264f
YunCloudy <cloudy.best@gmail.com>
2026-06-11 01:46:00 +0800

    Add files via upload

--- v3/app.js
+++ v3/app.js
@@ -1857,6 +1857,7 @@ function renderAdmin() {
       <div class="cei-spots-section">
         <div class="cei-spots-header">
          <span class="cei-label">價格</span>
+         <div class="toggle-row">
            <span class="toggle-label" id="newShowPriceLabel">隱藏價格</span>
            <label class="toggle">
              <input type="checkbox" id="newShowPrice">
